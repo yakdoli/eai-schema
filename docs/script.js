23cd3ea2157d91e9982b5dd7df8ef4d1395f7137
Yakdol Jo <ourpebble@gmail.com>
2025-09-03 14:26:12 +0900

    feat: add dark mode

--- docs/script.js
+++ docs/script.js
@@ -81,6 +81,28 @@ class EAISchemaApp {
         this.closeSettings();
       }
     });
+
+    // Dark mode
+    const toggleSwitch = document.querySelector("#checkbox");
+    const currentTheme = localStorage.getItem("theme");
+
+    if (currentTheme) {
+      document.body.classList.toggle("dark-mode", currentTheme === "dark");
+
+      if (currentTheme === "dark") {
+        toggleSwitch.checked = true;
+      }
+    }
+
+    toggleSwitch.addEventListener("change", (e) => {
+      if (e.target.checked) {
+        document.body.classList.add("dark-mode");
+        localStorage.setItem("theme", "dark");
+      } else {
+        document.body.classList.remove("dark-mode");
+        localStorage.setItem("theme", "light");
+      }
+    });
   }
 
   setupTabs() {
