058007a98ad7132069ece35945e76c0e5c87e7c5
Yakdol Jo <ourpebble@gmail.com>
2025-09-06 11:20:20 +0900

    config: Update .gitignore and test environment configuration

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('jest').Config} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   roots: ['<rootDir>'],
   testMatch: [
     '<rootDir>/src/**/__tests__/**/*.test.ts',
